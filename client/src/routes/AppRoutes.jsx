fcc70e5228620027471068e23899b88414347be2
Jarrad <bluejay.jzp@gmail.com>
2023-09-05 11:53:08 +1000

    changed login route to a page not a component

--- client/src/routes/AppRoutes.jsx
+++ client/src/routes/AppRoutes.jsx
@@ -1,14 +1,15 @@
 import { Routes, Route } from "react-router-dom";
 import ErrorPage from "../pages/ErrorPage";
-import SignIn from "../components/SignInForm";
+import SignInForm from "../components/SignInForm";
 import CreateAccountForm from "../components/CreateAccountForm";
 import TestPage from "../pages/TestPage";
+import LoginPage from "../pages/LoginPage";
 
 function AppRoutes(props) {
     return (
         <Routes>
             <Route index element={<TestPage />} />
-            <Route path="/login" element={<SignIn />} />
+            <Route path="/login" element={<LoginPage />} />
             <Route path="/signup" element={<CreateAccountForm />} />
             <Route path="*" element={<ErrorPage />} />
         </Routes>
